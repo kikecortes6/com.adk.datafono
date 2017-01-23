93dcd2b92ff06f4081a0dfcba1433ed3f3b52f4b
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-23 16:35:21 -0500

    Update datacom.js

--- www/datacom.js
+++ www/datacom.js
@@ -1,1 +1,22 @@
 
+module.exports = (function() {
+
+  var _test = function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "test", []);
+    };
+  var _opengps =function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "openGps", []);
+    };
+  var _subscribe =function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "Subscribe", []);
+    };
+  
+   return {
+    test: _test,
+    openGps: _opengps,
+    subscribe:_subscribe
+  
+
+  };
+
+})();
