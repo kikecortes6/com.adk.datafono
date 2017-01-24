07d5ec337e48242c6990ee149fce48d951cf9dd6
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-24 16:48:04 -0500

    Update Datafono.java

--- src/android/Datafono.java
+++ src/android/Datafono.java
@@ -6,4 +6,16 @@ import com.ingenico.pclservice.*;
 import android.app.*;
 
 public class Datafono extends Activity {
+  // Declare PclService interface
+
+protected PclService mPclService = null;
+
+// Declare Serviceconnection
+  
+  private PclServiceConnection mServiceConnection;
+  
+  
+  
+  
+  
 };
