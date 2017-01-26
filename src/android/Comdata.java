39e0dd1f6c794a6f2f71bf77c5f3ddf67b78668a
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-26 10:59:42 -0500

    Update Comdata.java

--- src/android/Comdata.java
+++ src/android/Comdata.java
@@ -22,10 +22,11 @@ import android.content.ServiceConnection;
 
 public class Comdata extends CordovaPlugin {
  private CallbackContext callbackContext;
-      private String test="hola Mundo";
- private String ser="Servicio Iniciado";
+      private String test="Conexion Iniciada";
+ private String ser="Servicio Finalizado";
  private String saludo;
   private Datafono datafono;
+  protected PclService mPclService;
   public Activity getActivity() {
     return this.cordova.getActivity();
   }
@@ -50,10 +51,20 @@ public class Comdata extends CordovaPlugin {
           callbackContext.success(test);
 
         }else if(action.equals("testing")){
-         saludo=test+args;
-        callbackContext.success(saludo);
+
+          boolean bRet= Datafono.getInstance().isCompanionConnected();
+            if(bRet==true){
+              callbackContext.success("Connected");
+
+            }else{
+              callbackContext.success("Not Connected");
+            }
+
+
+
 
        }else if(action.equals("pcl")){
+         Datafono.getInstance().releaseService(getActivity());
 
          callbackContext.success(ser);
 
