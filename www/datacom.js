b2d92c8e0c5a7e9e436fc1a31b723d18c8f4f33a
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-26 11:14:43 -0500

    Update datacom.js

--- www/datacom.js
+++ www/datacom.js
@@ -10,11 +10,23 @@ module.exports = (function() {
   var _disconnect =function (successCallback, errorCallback) {
       cordova.exec(successCallback, errorCallback, "Comdata", "finish", []);
     };
+  var _fFunction = function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "f", []);
+    };
+  var _sFunction =function (args,successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "s", [args]);
+    };
+  var _tFunction =function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Comdata", "t", []);
+    };
   
    return {
     connect: _connect,
     checkConnection: _checkConnection,
-    disconnect:_disconnect
+    disconnect:_disconnect,
+    fFunction:_fFunction,
+    sFunction:_sFunction,
+    tFunction:_tFunction
   
 
   };
