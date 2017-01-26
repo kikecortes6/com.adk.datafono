882ef3ef05557dbed99f0af3a60622c7e3a7949b
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-26 11:04:35 -0500

    Update Comdata.java

--- src/android/Comdata.java
+++ src/android/Comdata.java
@@ -1,5 +1,4 @@
 package com.ci24.datafono;
-
 import com.ci24.functions.Datafono;
 import com.ingenico.pclutilities.*;
 import com.ingenico.pclservice.*;
@@ -9,63 +8,41 @@ import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.IBinder;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 
-
-
-
-
 public class Comdata extends CordovaPlugin {
  private CallbackContext callbackContext;
-      private String test="Conexion Iniciada";
+ private String test="Conexion Iniciada";
  private String ser="Servicio Finalizado";
  private String saludo;
   private Datafono datafono;
   protected PclService mPclService;
-  public Activity getActivity() {
-    return this.cordova.getActivity();
-  }
+ 
+  public Activity getActivity() {    return this.cordova.getActivity();  }
+  private Intent getIntent() {     return getActivity().getIntent();   }
+  private void setIntent(Intent intent) {     getActivity().setIntent(intent);   }
 
-  private Intent getIntent() {
-    return getActivity().getIntent();
-  }
-
-  private void setIntent(Intent intent) {
-    getActivity().setIntent(intent);
-  }
-
-    @Override
+ @Override
     public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) {
 
-
-
-       if (action.equals("test")) {
-
+       if (action.equals("init")) {
          Datafono.getInstance().initService(getActivity());
-
           callbackContext.success(test);
-
-        }else if(action.equals("testing")){
-
+        
+        }else if(action.equals("check")){
           boolean bRet= Datafono.getInstance().isCompanionConnected();
             if(bRet==true){
               callbackContext.success("Connected");
-
             }else{
               callbackContext.success("Not Connected");
             }
 
-
-
-
-       }else if(action.equals("pcl")){
+       }else if(action.equals("finish")){
          Datafono.getInstance().releaseService(getActivity());
-
          callbackContext.success(ser);
 
        }
