c7cc1eadce3937242a97ac822d67e06540ea28ca
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-26 09:16:35 -0500

    Update Comdata.java

--- src/android/Comdata.java
+++ src/android/Comdata.java
@@ -9,6 +9,9 @@ import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.IBinder;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
@@ -22,35 +25,49 @@ public class Comdata extends CordovaPlugin {
       private String test="hola Mundo";
  private String ser="Servicio Iniciado";
  private String saludo;
+  private Datafono datafono;
+  public Activity getActivity() {
+    return this.cordova.getActivity();
+  }
+
+  private Intent getIntent() {
+    return getActivity().getIntent();
+  }
+
+  private void setIntent(Intent intent) {
+    getActivity().setIntent(intent);
+  }
 
     @Override
     public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) {
-      
-     
-  
+
+
+
        if (action.equals("test")) {
-      
+
+         Datafono.getInstance().initService(getActivity());
+
           callbackContext.success(test);
-    
+
         }else if(action.equals("testing")){
          saludo=test+args;
         callbackContext.success(saludo);
-        
-       }else if(action.equals("pcl")){
-       Datafono.initService();
-         callbackContext.success(ser);
-        
-       }
-        
-        
-      
 
-        
+       }else if(action.equals("pcl")){
+
+         callbackContext.success(ser);
+
+       }
+
+
+
+
+
 
         return true;
-        
-    }
-    
 
-    
+    }
+
+
+
 }
