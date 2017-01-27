31d16c20eba40ed96319b227bb7acfdafacfb81a
kikecortes6 <u1801864@unimilitar.edu.co>
2017-01-27 08:59:39 -0500

    Update Comdata.java

--- src/android/Comdata.java
+++ src/android/Comdata.java
@@ -13,6 +13,9 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+import android.util.Log;
+
+import static org.apache.cordova.device.Device.TAG;
 
 public class Comdata extends CordovaPlugin {
  private CallbackContext callbackContext;
@@ -21,7 +24,7 @@ public class Comdata extends CordovaPlugin {
  private String saludo;
   private Datafono datafono;
   protected PclService mPclService;
- 
+
   public Activity getActivity() {    return this.cordova.getActivity();  }
   private Intent getIntent() {     return getActivity().getIntent();   }
   private void setIntent(Intent intent) {     getActivity().setIntent(intent);   }
@@ -32,10 +35,10 @@ public class Comdata extends CordovaPlugin {
        if (action.equals("init")) {
          Datafono.getInstance().initService(getActivity());
           callbackContext.success(test);
-        
+
         }else if(action.equals("check")){
-          boolean bRet= Datafono.getInstance().isCompanionConnected();
-            if(bRet==true){
+          boolean check= Datafono.getInstance().isCompanionConnected();
+            if(check==true){
               callbackContext.success("Connected");
             }else{
               callbackContext.success("Not Connected");
@@ -46,13 +49,56 @@ public class Comdata extends CordovaPlugin {
          callbackContext.success(ser);
 
        }else if(action.equals("f")){
-      
+      boolean barC=Datafono.getInstance().openBarCode();
+         if(barC==true){
+           callbackContext.success("Barcode Open");
+         }else{
+           callbackContext.success("Error Opening Barcode");
+         }
        }
      else if(action.equals("s")){
-       
+         int appNumber=0;
+         TransactionIn transIn = new TransactionIn();
+         TransactionOut transOut = new TransactionOut();
+      //   transIn.setAmount("000000500000");
+      //   transIn.setCurrencyCode("978");
+      //   transIn.setOperation("C");
+      //   transIn.setTermNum("58");
+      //   transIn.setAuthorizationType("0");
+      //   transIn.setCtrlCheque("0");
+      //   transIn.setUserData1("Oscar");
+
+
+        // byte[] extDataIn = new byte[0];
+         byte extDataIn[]=new byte[]{(byte)0xFC,(byte)0x56,(byte)0xDF,(byte)0xFF,(byte)0x25,(byte)0x0A,(byte)0x37,(byte)0x36,(byte)0x44,(byte)0x46,(byte)0x42,(byte)0x46,(byte)0x37,(byte)0x33,(byte)0x42,(byte)0x35,(byte)0x9A,(byte)0x03,(byte)0x17,(byte)0x01,(byte)0x26,(byte)0x9F,(byte)0x21,(byte)0x03,(byte)0x12,(byte)0x28,(byte)0x00,(byte)0xDC,(byte)0x01,(byte)0x0A,(byte)0xDF,(byte)0xFE,(byte)0x53,(byte)0x0A,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x32,(byte)0x20,(byte)0x20,(byte)0x20,(byte)0x20,(byte)0xDF,(byte)0xFF,(byte)0x2B,(byte)0x0A,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x30,(byte)0x31,(byte)0x20,(byte)0x20,(byte)0x20,(byte)0x20,(byte)0xDF,(byte)0xFE,(byte)0x40,(byte)0x06,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x09,(byte)0x00,(byte)0x00,(byte)0xDF,(byte)0xFF,(byte)0x22,(byte)0x06,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x08,(byte)0x97,(byte)0x98,(byte)0xDF,(byte)0xFF,(byte)0x23,(byte)0x06,(byte)0x00,(byte)0x00,(byte)0x08,(byte)0x79,(byte)0x87,(byte)0x98};
+         byte[] extDataOut = new byte[8000];
+         long[] sizebuff=new long[8000];
+
+         int sizeIn=88;
+
+
+         Datafono.getInstance().doTransactionEx(transIn,transOut,appNumber,extDataIn,sizeIn,extDataOut,sizebuff);
+
+
+         callbackContext.success("transacition");
+
+
        }
      else if(action.equals("t")){
-        
+         TransactionIn transIn = new TransactionIn();
+         TransactionOut result=new TransactionOut();
+
+
+         String amount = "995";
+         transIn.setAmount(amount);
+         transIn.setCurrencyCode("978");
+         transIn.setOperation("C");
+         transIn.setTermNum("58");
+         Log.d(TAG,"envio Tramas!!!!");
+         boolean trans = Datafono.getInstance().doTransaction(transIn,result);
+
+         callbackContext.success("getInfo");
+
        }
 
 
