63033621712c5715a38be49421cfb90bd7984a9d
kikecortes6 <u1801864@unimilitar.edu.co>
2017-02-15 14:32:29 -0500

    Update IfaceCallbackDatafono.java

--- src/android/IfaceCallbackDatafono.java
+++ src/android/IfaceCallbackDatafono.java
@@ -7,5 +7,8 @@ import org.json.JSONArray;
 public interface IfaceCallbackDatafono {
 
   void responseDatafono(JSONArray response);
+  void disconnect();
+  void getDevices(JSONArray devices);
 
+  void connect();
 }
