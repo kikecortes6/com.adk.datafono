31f184a71a1c2ecc760ad265303ef98bedc6228c
kikecortes6 <u1801864@unimilitar.edu.co>
2017-02-20 15:36:16 -0500

    Update Datafono.java

--- src/android/Datafono.java
+++ src/android/Datafono.java
@@ -32,12 +32,12 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.Charset;
 import java.util.Set;
-
-
 import static org.apache.cordova.device.Device.TAG;
 
 
+//clase la cual utilizaremos para todos los procesos de comunicacióny ejecución
 public class Datafono  {
+  //inicialización de variables
   protected PclService mPclService = null;
   private IfaceCallbackDatafono callback;
   private static Boolean m_BarCodeActivated = false;
@@ -47,16 +47,18 @@ public class Datafono  {
   private PclServiceConnection mServiceConnection;
   int SN, PN;
   String pack="com.ionicframework.plugindata691535";
+  public boolean dis=true;
+  protected _SYSTEMTIME sysTime;
+  protected boolean mBound = false;
 
-
-
+  //función que retorna la instancia de la clase para poder ser utilizada
   public static Datafono getInstance(){
     if(datafono == null){
         datafono = new Datafono();
     }
     return datafono;
   }
-
+   // Función que empieza el servicio de pcl
    public void startPclService(Activity act) {
     if (!mServiceStarted){
       Intent i = new Intent(act, PclService.class);
@@ -69,12 +71,9 @@ public class Datafono  {
     }
 
   }
-    public boolean dis=true;
+    //Función de chequeo de conexión
   public boolean isCompanionConnected()   {
     boolean bRet = false;
-
-
-
       if (mPclService != null) {
         byte result[] = new byte[1];
         {
@@ -84,31 +83,16 @@ public class Datafono  {
           }
         }
       }
-
       return bRet;
-
   }
 
-
-
-  public void stopPclService(Activity activity) {
-     if(mServiceStarted){
-      Intent i = new Intent(activity, PclService.class);
-      if (activity.getApplicationContext().stopService(i)) {
-        mServiceStarted=false;
-
-      }
-    }
-  }
-
+  //función que retorna los dispositivos
   public void getDevices(Activity act,IfaceCallbackDatafono callback) {
     try{
-
       Set<PclUtilities.BluetoothCompanion> btComps = mPclUtil.GetPairedCompanions();
       JSONArray devicesResponse=new JSONArray();
       this.callback=callback;
       String data=null;
-
       if (btComps != null && (btComps.size() > 0)) {
        int i=0;
         for (PclUtilities.BluetoothCompanion comp : btComps) {
@@ -123,15 +107,14 @@ public class Datafono  {
       Log.d(TAG,e.toString());
     }
   }
-
+  //Función que unicializa la el estado de conexión
   public void initialize(Activity activity,String appName) {
     pack=appName;
     mPclUtil=new PclUtilities(activity,pack,"pairing_addr.txt");
-
   }
 
+  //clase para la lectura de la fecha y hora
   public class _SYSTEMTIME   {
-    // WORD = UInt16
     public short wYear;
     public short wMonth;
     public short wDayOfWeek;
@@ -142,14 +125,13 @@ public class Datafono  {
     public short wMilliseconds;
   }
 
-  protected _SYSTEMTIME sysTime;
 
+    //Función para obterner fecha y hora
   public String getTime() {
     boolean ret = false;
     byte[] time = new byte[16];
     String dateTime=null;
     if( mPclService != null ) {
-
         sysTime = new _SYSTEMTIME();
         ret = mPclService.getTerminalTime(time);
         ByteBuffer bbTime = ByteBuffer.wrap(time);
@@ -163,29 +145,20 @@ public class Datafono  {
         sysTime.wSecond = bbTime.getShort();
         sysTime.wMilliseconds = bbTime.getShort();
         dateTime= String.valueOf(sysTime.wHour)+":"+String.valueOf(sysTime.wMinute)+":"+String.valueOf(sysTime.wSecond)+" "+String.valueOf(sysTime.wYear)+"/"+String.valueOf(sysTime.wMonth)+"/"+String.valueOf(sysTime.wDay);
-
-
       return dateTime;
-
-
-
     }else{
       return "0";
     }
-
-
-
   }
 
+  //función que retorna los seriales del dispositivo
   public String getTermInfo() {
     boolean ret = false;
     byte[] serialNbr = new byte[4];
     byte[] productNbr = new byte[4];
     String info=null;
     if( mPclService != null ) {
-
         ret = mPclService.getTerminalInfo(serialNbr, productNbr);
-
         ByteBuffer bbSN = ByteBuffer.wrap(serialNbr);
         ByteBuffer bbPN = ByteBuffer.wrap(productNbr);
         bbSN.order(ByteOrder.LITTLE_ENDIAN);
@@ -196,112 +169,48 @@ public class Datafono  {
         Log.d(TAG,String.valueOf(SN));
         Log.d(TAG,String.valueOf(PN));
         info=String.valueOf(SN)+"//"+String.valueOf(PN);
-
       return info;
     }else {
-
-
       return "0";
     }
-
-
   }
+
+  //Función que retorna el porcentaje de bateria restante
   public int getBatteryLevel(){
     int level=0;
     int t[]= new int[1];
     if(mPclService!=null) {
-
-
       mPclService.getBatteryLevel(t);
       level = t[0];
       return level;
     }else {
       return -1;
     }
-
   }
+  //Funcion sin implementar
   public boolean printText( String strText ) {
-
     boolean Result = false;
-
-
-
     byte[] PrintResult = new byte[1];
-
     try {
-
       Result = mPclService.printText(strText, PrintResult);
-
     } catch (Exception e) {
-
       e.printStackTrace();
-
     }
     return Result;
-
   }
 
+  //Función sin implementar
   public boolean doTransaction(TransactionIn transIn, TransactionOut transOut) {
     boolean ret = false;
-
     if( mPclService != null ) {
       {
         ret = mPclService.doTransaction(transIn, transOut);
       }
     }
     return ret;
-
   }
 
-  private IPclServiceCallback mCallback = new IPclServiceCallback() {
-
-    public int shouldEndReceipt() {
-      return 0;
-    }
-    @Override
-    public void signatureTimeoutExceeded() {
-
-    }
-    public void shouldCutPaper() {
-
-    }
-    @Override
-    public void shouldDoSignatureCapture(int pos_x, int pos_y, int width,
-                                         int height, int timeout) {
-
-    }
-
-    public int shouldStartReceipt(byte type) {
-      return 0;
-    }
-    @Override
-    public void shouldAddSignature() {
-
-    }
-
-    public void shouldFeedPaper(int lines) {
-
-// Update application TextView for instance
-
-    }
-    public void shouldPrintRawText(byte[] text, byte charset, byte font, byte justification, byte xfactor, byte yfactor, byte underline, byte bold) {
-
-    }
-
-//Implement all other callbacks
-
-    public void shouldPrintText(String text, byte font, byte justification, byte xfactor, byte yfactor, byte underline, byte bold) {
-
-    }
-    public void shouldPrintImage(Bitmap image, byte justification){
-
-    }
-
-
-
-
-  };
-
+  //Función que realiza la transacción
   public void doTransactionEx(IfaceCallbackDatafono callback, TransactionIn transIn, TransactionOut transOut, int appNumber, byte[] extDataIn, int inBufferSize, byte[] extDataOut, long[] extDataOutSize) {
     this.callback = callback;
     if( mPclService != null ) {
@@ -312,7 +221,7 @@ public class Datafono  {
       }
     }
   }
-
+   //Clase para realizar la transacción
   class DoTransactionExTask extends AsyncTask<Void, Void, Boolean> {
     private TransactionIn transIn;
     private TransactionOut transOut;
@@ -320,7 +229,6 @@ public class Datafono  {
     private byte[] extDataOut;
     private long[] extDataOutSize;
     private int appNumber;
-
     public DoTransactionExTask(TransactionIn transIn, TransactionOut transOut, int appNumber, byte[] extDataIn, byte[] extDataOut) {
       this.transIn = transIn;
       this.transOut = transOut;
@@ -330,12 +238,10 @@ public class Datafono  {
       this.extDataOutSize = new long[1];
       this.extDataOutSize[0] = extDataOut.length;
     }
-
     protected Boolean doInBackground(Void... tmp) {
       Boolean ret=mPclService.doTransactionEx(transIn, transOut, appNumber, extDataIn, extDataIn.length, extDataOut, extDataOutSize);
       return ret;
     }
-
     protected void onPostExecute(Boolean result) {
       JSONArray respuesta = new JSONArray();
       int responseSize=0;
@@ -377,19 +283,18 @@ public class Datafono  {
   }
 
 
-
+   //Función sin implementar
   public boolean openBarCode() {
     Log.d(TAG, "openBarCode" );
     if((mPclService != null) && !m_BarCodeActivated)
       m_BarCodeActivated = setBarCodeActivation(true);
-
     return m_BarCodeActivated;
   }
 
+  //Función sin implementar
   private boolean setBarCodeActivation(boolean activateBarCode){
     boolean result = false;
     byte array [] = null;
-
     if(mPclService != null)
     {
       array = new byte[1];
@@ -409,30 +314,24 @@ public class Datafono  {
           result = true;
         }
       }
-
     }
-
     return result;
   }
-
+    //Clase para los servición y conecxión pcl
   public class PclServiceConnection implements ServiceConnection {
-
         public void onServiceConnected(ComponentName className, IBinder boundService ){
           PclService.LocalBinder binder = (PclService.LocalBinder) boundService;
           mPclService = (PclService) binder.getService();
           Log.d(TAG, "onServiceConnected" );
           callback.connect();
-
            }
-
         public void onServiceDisconnected(ComponentName className) {
                 mPclService = null;
                 Log.d(TAG, "onServiceDisconnected" );
             }
-
     };
 
-
+    //Conectar un dispositivo
 public void pairCompanion(Activity act,String macAddress,IfaceCallbackDatafono callback){
 try{
   this.callback=callback;
@@ -446,112 +345,26 @@ try{
 }catch (Exception e){
   Log.d(TAG,e.toString());
 }
-
-
 }
-  public void startCompanion(Activity act){
-    try{
-      mPclUtil=new PclUtilities(act,pack,"pairing_addr.txt");
-      Set<PclUtilities.BluetoothCompanion> btComps = mPclUtil.GetPairedCompanions();
-      String device = null;
-      boolean bFound = false;
-
-      if (btComps != null && (btComps.size() > 0)) {
-        // Loop through paired devices
-        for (PclUtilities.BluetoothCompanion comp : btComps) {
-          Log.d(TAG, comp.getBluetoothDevice().getAddress());
-          Log.d(TAG,comp.getBluetoothDevice().getName());
-          device=comp.getBluetoothDevice().getAddress();
-          if(comp.isActivated()){
-            bFound=true;
-          }
-
-          //   radioButton.setText(comp.getBluetoothDevice().getAddress() + " - " + comp.getBluetoothDevice().getName());
-
-        //  device=comp.getBluetoothDevice().getName();
- /*         if(comp.activate()){
-            Log.d(TAG,"dispositivo activado........");
-          }*/
-
-
-
-
-        }
-      }
-      String activated=null;
-
-      if (!bFound) {
-        activated = mPclUtil.getActivatedCompanion();
-
-
-        Log.d(TAG,activated);
-      }
-
-
-
-      Log.d(TAG,btComps.toString());
-      try{
-      //  Log.d(TAG,device);
-      //  Log.d(TAG,device.substring(0, 17));
-
-        int f= mPclUtil.ActivateCompanion(device);
-        Log.d(TAG, String.valueOf(f));
-
-
-      }catch (Exception e){
-        Log.d(TAG,e.toString());
-
-      }
-
-      try{
-        startPclService(act);
-
-      }catch (Exception e){
-        Log.d(TAG,e.toString());
-
-      }
-      try {
-       // initService(act);
-
-      }catch (Exception e){
-        Log.d(TAG,e.toString());
-
-      }
-
-
-
-    }catch (Exception e){
-      Log.d(TAG,e.toString());
-    }
-  }
-
-  protected boolean mBound = false;
+  //Función para inicializar el servicio
 public  void  initService(Activity act){
   if (!mBound) {
     try {
-
-
       mServiceConnection = new PclServiceConnection();
-
-
     } catch (Exception e) {
       Log.d(TAG, e.toString());
     }
-
     Intent intent = new Intent(act, PclService.class);
     intent.putExtra("PACKAGE_NAME", pack);
     intent.putExtra("FILE_NAME", "pairing_addr.txt");
     try {
       mBound=act.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
-
     } catch (Exception e) {
       Log.d(TAG, e.toString());
     }
-
   }
 }
-  // You can call this method in onDestroy for instance
-
+  //Función para liberar el servicio
 public void releaseService(Activity act,IfaceCallbackDatafono callback){
    this.callback=callback;
   if(mBound){
@@ -563,17 +376,11 @@ public void releaseService(Activity act,IfaceCallbackDatafono callback){
     act.unbindService(mServiceConnection);
     mBound=false;
   }
-
   Log.d(TAG, "onServiceDisconnected" );
   dis=false;
   mPclService=null;
   callback.disconnect();
 }
-
-
-
-
-
 
 }
 
